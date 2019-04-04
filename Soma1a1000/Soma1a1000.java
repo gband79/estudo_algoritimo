b30b330991fe6e10acbb9ef4d3cc800d953e7785
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-04-04 14:45:23 -0300

    Correção Lucas

--- Soma1a1000/Soma1a1000.java
+++ Soma1a1000/Soma1a1000.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
-
 public class Soma1a1000{
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         int numero = 1;
         int somaDosNumeros = 0;
         for(int i=1; i<=1000; i++){
-            numero = scan.nextInt();
             somaDosNumeros += numero;
+            numero++;
         }
     
-    scan.close();
-    System.out.println("A some dos números entre 1 e 1000 dá: "+somaDosNumeros);
+    System.out.println("A some dos numeros entre 1 e 1000 ate: "+somaDosNumeros);
     }
 } 
