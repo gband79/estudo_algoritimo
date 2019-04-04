9030b9c14cc5dd5c198546d654f8bd025e662cea
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-04-04 14:59:10 -0300

    Aplicacao de boas praticas

--- Soma1a1000/Soma1a1000.java
+++ Soma1a1000/Soma1a1000.java
@@ -1,13 +1,18 @@
 public class Soma1a1000{
 
+	/*
+	 * Enunciado:
+	 * Faça um programa em Java que imprima a soma dos números de 1 até 1000.
+	 */
+	
     public static void main(String[] args) {
-        int numero = 1;
+        int numero = 0;
         int somaDosNumeros = 0;
         for(int i=1; i<=1000; i++){
+        	numero++;
             somaDosNumeros += numero;
-            numero++;
         }
-    
     System.out.println("A some dos numeros entre 1 e 1000 ate: "+somaDosNumeros);
     }
 } 
+
