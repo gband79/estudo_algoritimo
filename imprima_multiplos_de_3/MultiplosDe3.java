1965d45a29ac5b35149427b4e3f19703cb0ec668
Gabriel Morais Bandeira <gmbandeira@stefanini.com>
2019-04-08 16:22:25 -0300

    code not reviewed

---
+++ imprima_multiplos_de_3/MultiplosDe3.java
@@ -0,0 +1,25 @@
+
+/*
+Faça um programa em Java que imprima todos os múltiplos de 3, entre 1 e 100.
+*/
+
+//Lucas faz o favor tenta rodar aí. Não deu erro mas tb nume deu nenhum nada de retorno
+
+public class MultiplosDe3
+{
+    
+    public static void main(String[] args) 
+    {
+        int numero = 100;
+        int multiplosDe = 3; 
+        for(int i=100; i>=1; i--)
+        { 
+            if(numero % multiplosDe == 0)
+            {
+                numero--;
+                numero /= multiplosDe;
+                System.out.println("Multiplo de 3: "+ numero);
+            }
+        } 
+    }
+}
