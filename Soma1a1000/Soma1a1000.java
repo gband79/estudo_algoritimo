8c75550d3dc523d08a490e7cfbd45974ab197232
gband79 <gbandeira79@yahoo.com.br>
2020-02-05 16:56:52 -0300

    Updating Soma1a000.java

--- Soma1a1000/Soma1a1000.java
+++ Soma1a1000/Soma1a1000.java
@@ -8,7 +8,7 @@ public class Soma1a1000{
     public static void main(String[] args) {
         int numero = 1;
         int somaDosNumeros = 0;
-        for(int i=1; i<=10; i++){
+        for(int i=1; i<=1000; i++){
         	numero++;
             somaDosNumeros += numero;
         }
